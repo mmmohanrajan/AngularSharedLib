6882bef9989e91d5978bc8207a953faf264376af
Mohanrajan <mohanrajan.m@comfyapp.com>
2023-06-25 12:01:45 +0530

    first commit

---
+++ projects/popup-lib/src/lib/my-library/popup/popup.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-popup',
+  templateUrl: './popup.component.html',
+  styleUrls: ['./popup.component.css']
+})
+export class PopupComponent {
+
+}
---
+++ projects/popup-lib/src/public-api.ts
@@ -0,0 +1,7 @@
+/*
+ * Public API Surface of popup-lib
+ */
+
+export * from './lib/my-library/popup/popup.component';
+export * from './lib/test/test.module';
+export * from './lib/test/test.component';
